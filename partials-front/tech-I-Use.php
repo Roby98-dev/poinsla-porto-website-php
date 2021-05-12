b3885a07c400d6e23baea1306e88f72bbe27ebda
Roby98-dev <bbmrobi3@gmail.com>
2021-05-12 13:15:37 +0800

    Updated at 16:34 in 12 mei 2021

--- partials-front/tech-I-Use.php
+++ partials-front/tech-I-Use.php
@@ -1,10 +1,10 @@
-<section class="categories">
-    <h2 class="text-center text-uppercase mt-4 mb-3">Tech I Use</h2>
+<section class="categories bg-dark pt-4">
+    <h2 class="text-center text-uppercase text-light mb-3">Tech I Use</h2>
     <div class="container pb-5">
         <div class="row">
 
             <?php
-            $sql = "SELECT * FROM tbl_category WHERE active='Yes' AND featured='Yes' LIMIT 4";
+            $sql = "SELECT * FROM tbl_category WHERE active='Yes' AND featured='Yes' ORDER BY id DESC LIMIT 4";
 
             $res = mysqli_query($conn, $sql);
             $count = mysqli_num_rows($res);
@@ -20,9 +20,9 @@
 
                     <div class="col-lg-3 col-md-12 col-sm-12 mt-3 d-flex justify-content-center">
                         <a class="hover" href="<?= SITEURL; ?>category-foods.php?category_id=<?= $id; ?>">
-                            <div class="tech-card card bg-light shadow">
+                            <div class="tech-card card bg-secondary shadow">
                                 <div class="card-body border border-info">
-                                    <h4 class="title text-dark text-center text-uppercase mt-2 mb-2"><?= $title; ?></h4>
+                                    <h4 class="title text-light text-center text-uppercase mt-2 mb-2"><?= $title; ?></h4>
                                 </div>
                             </div>
                         </a>
